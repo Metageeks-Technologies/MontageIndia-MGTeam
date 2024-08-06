cb5015074f4ced3181c61dfa3afbbe159d99bc2d
shiva-metageeks <shiva@metageeks.tech>
2024-08-06 10:31:57 +0530

    "patch method added"

--- server/src/index.ts
+++ server/src/index.ts
@@ -47,7 +47,7 @@ const corsOptions = {
     }
   },
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS','PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization']
 };
 
