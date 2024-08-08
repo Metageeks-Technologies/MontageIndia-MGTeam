39123dd564bcdb32289485b15d02d20de46f0306
shiva-metageeks <shiva@metageeks.tech>
2024-08-08 11:31:34 +0530

    "user added to the store"

--- client/src/app/redux/store.ts
+++ client/src/app/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from './reducer/cartSlice';
+import userReducer from './feature/user/slice';
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    user: userReducer,
   },
 });
 
