b9f6b6f509a07ed0f645f6a10e10c14aacd7bc7f
shiva-metageeks <shiva@metageeks.tech>
2024-08-13 18:16:44 +0530

    product for the user done

--- server/src/model/user/customer.ts
+++ server/src/model/user/customer.ts
@@ -1,105 +1,119 @@
-import mongoose, { Model } from 'mongoose';
+import mongoose, { Model } from "mongoose";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-import * as validator from 'validator';
+import * as validator from "validator";
 import type { TCustomer } from "../../types/user";
-import config from '@src/utils/config';
+import config from "@src/utils/config";
 
-const customerSchema = new mongoose.Schema<TCustomer>({
-  name: {
-    type: String,
-    required: [true, "please enter your name"],
-    maxlength: [30, "name can't exceed 30 characters"],
-    minlength: [4, "name should have more than 4 characters"],
-    trim: true,
-    default: "none"
-  },
-  username:{
-    type: String,
-    required: [true, "please enter your username"],
-    maxlength: [30, "username can't exceed 30 characters"],
-    minlength: [4, "username should have more than 4 characters"],
-    trim: true,
-    default: "none",
-    unique: true,
-  },
-  email:{
-    type: String,
-    required: [true, "please enter your email"],
-    unique: true,
-    validate: [validator.isEmail, "please enter a valid email"],
-  },
-  phone: { 
-    type: String,
-    minlength: [10, "phone number should have minimum 10 characters"],
-    maxlength: [10, "phone number should have maximum 10 characters"],
-    trim: true,
-    default: "none",
-  },
-  password: {
-    type: String,
-    minlength: [6, "password should have a minimum of 6 characters"],
-    select: false,
-  },
-  isDeleted: { type: Boolean, default: false },
-  resetPasswordToken:{ type: String,default:undefined,},
-  resetPasswordExpires:{ type: Number,default: undefined,},
-  purchasedProducts: [{
-    productId: { 
-        type: mongoose.Schema.Types.ObjectId, 
-        ref: 'Product' 
+const customerSchema = new mongoose.Schema<TCustomer>(
+  {
+    name: {
+      type: String,
+      required: [true, "please enter your name"],
+      maxlength: [30, "name can't exceed 30 characters"],
+      minlength: [4, "name should have more than 4 characters"],
+      trim: true,
+      default: "none",
     },
-    variantId:[ {type:String,required:true}],
-  }],
-  cart: [{type: mongoose.Schema.Types.ObjectId, ref: 'Product' }],
-  subscription: { 
-      subscriptionId: {type:String ,default:''},
-      PlanId: { type:String, default:'' },
+    username: {
+      type: String,
+      required: [true, "please enter your username"],
+      maxlength: [30, "username can't exceed 30 characters"],
+      minlength: [4, "username should have more than 4 characters"],
+      trim: true,
+      default: "none",
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: [true, "please enter your email"],
+      unique: true,
+      validate: [validator.isEmail, "please enter a valid email"],
+    },
+    phone: {
+      type: String,
+      minlength: [10, "phone number should have minimum 10 characters"],
+      maxlength: [10, "phone number should have maximum 10 characters"],
+      trim: true,
+      default: "none",
+    },
+    password: {
+      type: String,
+      minlength: [6, "password should have a minimum of 6 characters"],
+      select: false,
+    },
+    isDeleted: { type: Boolean, default: false },
+    resetPasswordToken: { type: String, default: undefined },
+    resetPasswordExpires: { type: Number, default: undefined },
+    purchasedProducts: [
+      {
+        productId: {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "Product",
+        },
+        variantId: [{ type: String, required: true }],
+      },
+    ],
+    cart: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    wishlist: [{ type: mongoose.Schema.Types.ObjectId, ref: "Product" }],
+    subscription: {
+      subscriptionId: { type: String, default: "" },
+      PlanId: { type: String, default: "" },
       credits: { type: Number, default: 0 },
       planValidity: { type: Date, default: Date.now() },
-      status: { type: String, default: 'inactive' }
-   },
-  purchaseHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Order' }],
-},{
-  timestamps: true
-});
+      status: { type: String, default: "inactive" },
+    },
+    purchaseHistory: [{ type: mongoose.Schema.Types.ObjectId, ref: "Order" }],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 // Define model interface
-interface CustomerModel extends Model<TCustomer> { }
+interface CustomerModel extends Model<TCustomer> {}
 
 // Hash the password before saving
-customerSchema.pre<TCustomer>('save', async function (next) {
-    if (!this.isModified('password')) return next();
+customerSchema.pre<TCustomer>("save", async function (next) {
+  if (!this.isModified("password")) return next();
 
-    try {
-        const salt = await bcrypt.genSalt(10);
-        if (typeof this.password === 'string') {
-            this.password = await bcrypt.hash(this.password, salt);
-        } else {
-            throw new Error('Password is not defined');
-        }
-        next();
-    } catch (err:any) {
-        next(err);
+  try {
+    const salt = await bcrypt.genSalt(10);
+    if (typeof this.password === "string") {
+      this.password = await bcrypt.hash(this.password, salt);
+    } else {
+      throw new Error("Password is not defined");
     }
+    next();
+  } catch (err: any) {
+    next(err);
+  }
 });
 
 // Create JWT token
 customerSchema.methods.createJWT = function (this: TCustomer) {
-    if (!config.customerJwtSecret) {
-        throw new Error("JWT_SECRET is not defined in the environment.");
-    }
-    return jwt.sign({ id: this._id }, config.customerJwtSecret, { expiresIn: config.jwtLifetime });
+  if (!config.customerJwtSecret) {
+    throw new Error("JWT_SECRET is not defined in the environment.");
+  }
+  return jwt.sign({ id: this._id }, config.customerJwtSecret, {
+    expiresIn: config.jwtLifetime,
+  });
 };
 
 // Compare password
-customerSchema.methods.comparePassword = async function (this: TCustomer, givenPassword: string) {
-    if (typeof this.password === 'string') {
-        const isMatch = await bcrypt.compare(givenPassword, this.password);
-        return isMatch;
-    } else {
-        throw new Error('Password is not defined');
-    }
+customerSchema.methods.comparePassword = async function (
+  this: TCustomer,
+  givenPassword: string
+) {
+  if (typeof this.password === "string") {
+    const isMatch = await bcrypt.compare(givenPassword, this.password);
+    return isMatch;
+  } else {
+    throw new Error("Password is not defined");
+  }
 };
 
-export default mongoose.model<TCustomer, CustomerModel>('Customer', customerSchema);
+export default mongoose.model<TCustomer, CustomerModel>(
+  "Customer",
+  customerSchema
+);
--- server/src/types/user.ts
+++ server/src/types/user.ts
@@ -1,41 +1,41 @@
-import mongoose,{ Document } from "mongoose";
+import mongoose, { Document } from "mongoose";
 import type { ProductItem } from "@src/model/product/order";
 
-
 export type TAdmin = Document & {
-    name: string;
-    username:string;
-    email: string;
-    password?: string;
-    role: 'superadmin' | 'admin';
-    category: [string] ;
-    mediaType: [string] ;
-    resetPasswordToken?: string | undefined;
-    resetPasswordExpires?: Number | undefined;
-    isDeleted: Boolean;
-    createJWT(): string;
-    comparePassword(givenPassword: string): Promise<boolean>;
-}
+  name: string;
+  username: string;
+  email: string;
+  password?: string;
+  role: "superadmin" | "admin";
+  category: [string];
+  mediaType: [string];
+  resetPasswordToken?: string | undefined;
+  resetPasswordExpires?: Number | undefined;
+  isDeleted: Boolean;
+  createJWT(): string;
+  comparePassword(givenPassword: string): Promise<boolean>;
+};
 
 export type TCustomer = Document & {
-    username: string;
-    email: string;
-    name: string; // Customer's full name
-    password: string;
-    isDeleted: boolean;
-    purchasedProducts:ProductItem[];
-    cart: mongoose.Types.ObjectId[]; // References to products in the cart
-    subscription: {
-        subscriptionId:string;
-        PlanId: string;
-        credits: number;
-        planValidity: Date;
-    } // References to subscription
-    purchaseHistory?: mongoose.Types.ObjectId[]; // References to orders
-    subscriptionHistory?: mongoose.Types.ObjectId[]; // References to subscriptions
-    phone?: string;
-    resetPasswordToken?: string | undefined;
-    resetPasswordExpires?: Number | undefined;
-    createJWT(): string;
-    comparePassword(givenPassword: string): Promise<boolean>;
-}
+  username: string;
+  email: string;
+  name: string; // Customer's full name
+  password: string;
+  isDeleted: boolean;
+  purchasedProducts: ProductItem[];
+  cart: mongoose.Types.ObjectId[]; // References to products in the cart
+  wishlist: mongoose.Types.ObjectId[]; // References to products in the wishlist
+  subscription: {
+    subscriptionId: string;
+    PlanId: string;
+    credits: number;
+    planValidity: Date;
+  }; // References to subscription
+  purchaseHistory?: mongoose.Types.ObjectId[]; // References to orders
+  subscriptionHistory?: mongoose.Types.ObjectId[]; // References to subscriptions
+  phone?: string;
+  resetPasswordToken?: string | undefined;
+  resetPasswordExpires?: Number | undefined;
+  createJWT(): string;
+  comparePassword(givenPassword: string): Promise<boolean>;
+};
