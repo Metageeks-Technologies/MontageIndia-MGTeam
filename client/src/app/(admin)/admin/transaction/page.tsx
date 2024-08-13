e03ef5ee0c6c334873ea9e22630f2d4f095b054d
sajidmeta <sajid@metageeks.tech>
2024-08-13 18:05:42 +0530

    navbar fixing

--- client/src/app/(admin)/admin/transaction/page.tsx
+++ client/src/app/(admin)/admin/transaction/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 import instance from "@/utils/axios";
+import { Pagination, Button, Spinner } from '@nextui-org/react';
 import React, { useEffect, useState } from "react";
 
 interface Transaction {
@@ -17,6 +18,10 @@ interface Transaction {
 const Page = () => {
   const [transaction, setTransaction] = useState<Transaction[]>([]);
   const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState<number>(1);
+  const [dataPerPage, setDataPerPage] = useState<number>(6);
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [totalPages, setTotalPages] = useState<number>(1);
 
   const fetchUsers = async () => {
     setLoading(true);
@@ -40,6 +45,28 @@ const Page = () => {
       <h1 className="text-2xl font-bold mb-6 text-gray-800">
         Transaction History
       </h1>
+     
+      <div className="flex justify-between items-center gap-4 flex-wrap my-6">
+        <input
+          type="text"
+          placeholder="Search"
+          className="border rounded px-4 py-2 w-full max-w-sm"
+          // value={searchTerm}
+          // onChange={handleSearch}
+        />
+        <div className="flex items-center flex-wrap gap-4">
+          <div>
+            <select className="border rounded px-4 py-2" >
+              <option value={6}>6 Data per page</option>
+              <option value={12}>12 Data per page</option>
+              <option value={24}>24 Data per page</option>
+            </select>
+          </div>
+        </div>
+      </div>
+
+
+
       <div className="bg-white shadow-md rounded-lg">
         <div className="overflow-x-auto lg:overflow-visible">
           <table className="min-w-full text-sm text-left text-gray-900">
@@ -79,6 +106,46 @@ const Page = () => {
             </tbody>
           </table>
         </div>
+
+        {totalPages > 1 && (
+          <div className="flex justify-center items-center gap-4 my-4">
+            <Button
+              size="sm"
+              disabled={currentPage === 1}
+              variant="flat"
+              className={`${
+                currentPage === 1 ? 'opacity-70' : 'hover:bg-webgreenHover'
+              } bg-webgreen-light text-white rounded-md font-bold`}
+              onPress={() => setCurrentPage((prev) => (prev > 1 ? prev - 1 : prev))}
+            >
+              Prev
+            </Button>
+            <Pagination
+              color="success"
+              classNames={{
+                item: 'w-8 h-8 text-small bg-gray-100 hover:bg-gray-300 rounded-md',
+                cursor: 'bg-webgreen hover:bg-webgreen text-white rounded-md font-bold',
+              }}
+              total={totalPages}
+              page={currentPage}
+              // onChange={handlePageChange}
+              initialPage={1}
+            />
+            <Button
+              size="sm"
+              disabled={currentPage === totalPages}
+              variant="flat"
+              className={`${
+                currentPage === totalPages ? 'opacity-70' : 'hover:bg-webgreenHover'
+              } bg-webgreen-light text-white rounded-md font-bold`}
+              onPress={() => setCurrentPage((prev) => (prev < totalPages ? prev + 1 : prev))}
+            >
+              Next
+            </Button>
+          </div>
+        )}
+
+
       </div>
     </div>
   );
--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,121 +1,105 @@
 "use client";
-import React from 'react';
-import { LiaHomeSolid } from "react-icons/lia";
-import { MdHelpOutline, MdOutlineGeneratingTokens } from 'react-icons/md';
-import { GrCatalogOption } from "react-icons/gr";
-import { RiPencilRuler2Line } from 'react-icons/ri';
-
-import { useState } from 'react';
-import { AiOutlineHeart, AiOutlineShoppingCart, AiOutlineUser, AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
-import { MdLanguage } from 'react-icons/md';
+import React, { useEffect, useState } from 'react';
+import { AiOutlineHeart, AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 import { IoMdArrowDropdown } from 'react-icons/io';
 import { useRouter } from 'next/navigation';
 import CartPopup from '../cart/cartPage';
-// import CartPopup from './cart/cartPage';
-// import CartPopup from './cart/cartPage';
+import { FaUserCircle } from 'react-icons/fa';
+import instance from '@/utils/axios';
+import { notifySuccess } from '@/utils/toast';
 
-
-const Sidebar = () =>
-{
+const Sidebar = () => {
   const router = useRouter();
-  const [ menuOpen, setMenuOpen ] = useState( false );
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [isUserOpen, setIsUserOpen] = useState(false);
+  const [currentUser, setCurrentUser] = useState<any>("");
 
+
+  const handleLogout = async () => {
+    try {
+      const response = await instance.get("/auth/user/logout");
+      notifySuccess(response.data.message);
+      setCurrentUser("");
+      router.push("/");
+    } catch (error) {
+      console.error("Error in logout:", error);
+    }
+  };
+
+  
+  useEffect(() => {
+    // Fetching user details
+    handleLogout()
+  }, []);
+
+
+
+  const handleUserIconClick = () => {
+    setIsUserOpen(!isUserOpen);
+  };
 
   return (
-    // <div className="fixed top-0 left-0 h-full z-30 w-20 border flex flex-col items-center bg-gray-100 text-gray-700 shadow-md">
-    //   <div className="my-4">
-    //     <div className="flex flex-col items-center space-y-8">
-    //       <div className="flex flex-col items-center">
-    //         <LiaHomeSolid className="h-8 w-8" />
-    //         <p className="text-xs mt-2">Home</p>
-    //       </div>
-    //       <div className="flex flex-col items-center">
-    //         <MdOutlineGeneratingTokens className="h-8 w-8" />
-    //         <p className="text-xs mt-2">Generate</p>
-    //       </div>
-    //       <div className="flex flex-col items-center">
-    //         <GrCatalogOption className="h-8 w-8" />
-    //         <p className="text-xs mt-2">Catalog</p>
-    //       </div>
-    //       <div className="flex flex-col items-center">
-    //         <RiPencilRuler2Line className="h-8 w-8" />
-    //         <p className="text-xs mt-2">Create</p>
-    //       </div>
-    //     </div>
-    //   </div>
-    //   <div className="mt-auto mb-4">
-    //     <div className="flex flex-col items-center">
-    //       <MdHelpOutline className="h-8 w-8" />
-    //       <p className="text-xs mt-2">Help</p>
-    //     </div>
-    //   </div>
-    // </div>
-
     <div className="flex items-center justify-between bg-white px-6 py-4 shadow-md">
       <div className=" flex items-center gap-5">
-        <img src={ '/images/logo.png' } alt="logo"
+        <img src='/images/logo.png' alt="logo"
           className='w-44 h-10 cursor-pointer'
-          onClick={ () => router.push( '/' ) }
+          onClick={() => router.push('/')}
         />
-        <div className="hidden lg:flex items-center space-x-4 ">
-
+        <div className="hidden lg:flex items-center space-x-4">
           <ul className="flex items-center space-x-4 cursor-pointer">
-            <li className="text-gray-700 hover:text-black" onClick={ () => router.push( '/' ) }>Images</li>
-            <li className="text-gray-700 hover:text-black" onClick={ () => router.push( '/video' ) }>Video</li>
-            <li className="text-gray-700 hover:text-black" onClick={ () => router.push( '/audio' ) }>Audio</li>
-            <li className="text-gray-700 hover:text-black">AI Generator</li>
-            <li className="text-gray-700 hover:text-black">Enterprise</li>
+            <li className="text-gray-700 hover:text-black" onClick={() => router.push('/')}>Images</li>
+            <li className="text-gray-700 hover:text-black" onClick={() => router.push('/video')}>Video</li>
+            <li className="text-gray-700 hover:text-black" onClick={() => router.push('/audio')}>Audio</li>
           </ul>
         </div>
       </div>
 
       <div className='lg:block md:hidden hidden'>
-        <div className="flex  items-center space-x-4">
+        <div className="flex items-center space-x-6">
           <div className="flex items-center text-gray-700">
             <span>0 Credits Available</span>
             <IoMdArrowDropdown className="ml-1" />
           </div>
-          <a href="#" className="text-gray-700 hover:text-black">Pricing</a>
-          <MdLanguage className="text-gray-700 w-6 h-6" />
-          <AiOutlineHeart className="text-gray-700 w-6 h-6" />
-          {/* <AiOutlineShoppingCart className="text-gray-700 w-6 h-6" /> */ }
-          {/* this is cart */ }
+          <AiOutlineHeart className="text-gray-700 w-7 h-7" />
           <CartPopup />
-          <AiOutlineUser onClick={ () => router.push( '/user-profile' ) } className="text-gray-700 w-6 h-6 cursor-pointer" />
+          <div className="relative">
+            <FaUserCircle onClick={handleUserIconClick} className="text-gray-700 w-10 h-10 cursor-pointer" />
+            {isUserOpen && (
+              <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg py-2 z-10">
+                <ul className='cursor-pointer'>
+                  <li className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-black cursor-pointer" onClick={() => router.push('/user-profile')}>View User Details</li>
+                  <li className="block px-4 py-2 text-gray-700 hover:bg-gray-100 hover:text-black cursor-pointer" onClick={() => router.push('/logout')}>Logout</li>
+                </ul>
+              </div>
+            )}
+          </div>
         </div>
       </div>
 
       <div className="flex lg:hidden items-center space-x-4">
         <AiOutlineHeart className="text-gray-700 w-6 h-6" />
-        {/* here also cart */ }
         <CartPopup />
-        <AiOutlineUser className="text-gray-700 w-6 h-6" />
-        <AiOutlineMenu className="text-gray-700 w-6 h-6 cursor-pointer" onClick={ () => setMenuOpen( true ) } />
+        <AiOutlineMenu className="text-gray-700 w-6 h-6 cursor-pointer" onClick={() => setMenuOpen(true)} />
       </div>
 
-      { menuOpen && (
+      {menuOpen && (
         <div className="fixed inset-0 bg-white z-50 flex flex-col p-4">
           <div className="flex justify-between items-center">
-
-            <AiOutlineClose className="text-gray-700 w-6 h-6 cursor-pointer" onClick={ () => setMenuOpen( false ) } />
+            <AiOutlineClose className="text-gray-700 w-6 h-6 cursor-pointer" onClick={() => setMenuOpen(false)} />
           </div>
-          <ul className="mt-4 space-y-3 ">
-            <li className="block text-gray-700 hover:text-black py-2" onClick={ () => router.push( '/' ) }>Images</li>
-            <li className="block text-gray-700 hover:text-black py-2" onClick={ () => router.push( '/video' ) }>Video</li>
-            <li className="block text-gray-700 hover:text-black py-2" onClick={ () => router.push( '/audio' ) }>Music</li>
+          <ul className="mt-4 space-y-3">
+            <li className="block text-gray-700 hover:text-black py-2" onClick={() => router.push('/')}>Images</li>
+            <li className="block text-gray-700 hover:text-black py-2" onClick={() => router.push('/video')}>Video</li>
+            <li className="block text-gray-700 hover:text-black py-2" onClick={() => router.push('/audio')}>Music</li>
             <li className="block text-gray-700 hover:text-black py-2">Templates</li>
             <li className="block text-gray-700 hover:text-black py-2">Blog</li>
             <li className="block text-gray-700 hover:text-black py-2">Enterprise</li>
             <li className="block text-gray-700 hover:text-black py-2">Pricing</li>
           </ul>
         </div>
-      ) }
+      )}
     </div>
-
   );
 };
 
-export default Sidebar ;
-
-
-
+export default Sidebar;
