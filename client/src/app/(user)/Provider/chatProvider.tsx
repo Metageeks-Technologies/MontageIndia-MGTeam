63aa42212b272d23a4f5e126a887215172572b37
hariom-verma-meta <hariom.verma@metageeks.tech>
2024-08-29 17:35:58 +0530

    chat completed

--- client/src/app/(user)/Provider/chatProvider.tsx
+++ client/src/app/(user)/Provider/chatProvider.tsx
@@ -1,9 +1,7 @@
 "use client";
 import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
 
-export function ChatProvider ( {children}: {children: React.ReactNode;} ) {
-    console.log( `${process.env.NEXT_PUBLIC_CLIENT_URL}`, `${process.env.NEXT_PUBLIC_PROERTY_ID}`, `${process.env.NEXT_APP_WIDGET_ID}` )
-    console.log("Caht provider")
+export function ChatProvider ( {children}: {children: React.ReactNode;} ) { 
     return (
         <>
             <TawkMessengerReact 
