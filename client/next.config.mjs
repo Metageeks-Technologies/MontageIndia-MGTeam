3e37508f7a1c315e3473f032cfbfca8ffc70beb8
hariom-verma-meta <hariom.verma@metageeks.tech>
2024-08-31 15:38:28 +0530

    chat done

--- client/next.config.mjs
+++ client/next.config.mjs
@@ -6,23 +6,23 @@ const nextConfig = {
   images: {
     domains: ['mi2-public.s3.amazonaws.com', `${process.env.NEXT_PUBLIC_AWS_BUCKET}.s3.amazonaws.com`],
   },
- async headers() {
-    return [
-      {
-        source: '/:path*',
-        headers: [
-          {
-            key: 'Content-Security-Policy',
-            value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com;",
-          },
-          {
-            key: 'Set-Cookie',
-            value: 'SameSite=None; Secure',
-          },
-        ],
-      },
-    ];
-  },
+//  async headers() {
+//     return [
+//       {
+//         source: '/:path*',
+//         headers: [
+//           {
+//             key: 'Content-Security-Policy',
+//             value: "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://translate.google.com https://translate.googleapis.com https://translate-pa.googleapis.com;",
+//           },
+//           {
+//             key: 'Set-Cookie',
+//             value: 'SameSite=None; Secure',
+//           },
+//         ],
+//       },
+//     ];
+//   },
 };
 
 export default nextConfig;
