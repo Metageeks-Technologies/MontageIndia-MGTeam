44f1e358f715986f1f64bcc3b8c3c568640a35f3
hariom-verma-meta <hariom.verma@metageeks.tech>
2024-09-08 12:48:00 +0530

    filters done

--- client/src/app/(user)/search/image/page.tsx
+++ client/src/app/(user)/search/image/page.tsx
@@ -2,162 +2,137 @@
 import Footer from "@/components/Footer";
 import ImageGallery from "@/components/Home/homeImage";
 import instance from "@/utils/axios";
-import { Button, Pagination, Spinner } from "@nextui-org/react";
-import { useEffect, useState } from "react";
-import { useAppDispatch, useAppSelector } from "@/app/redux/hooks";
-import { setImagePage } from "@/app/redux/feature/product/slice";
-import { useRouter, useSearchParams } from "next/navigation";
-import { clearKeywords } from "@/app/redux/feature/product/api";
+import {Button, Pagination, Spinner} from "@nextui-org/react";
+import {useEffect, useState} from "react";
+import {useAppDispatch, useAppSelector} from "@/app/redux/hooks";
+import {setImagePage} from "@/app/redux/feature/product/slice";
+import {useRouter, useSearchParams} from "next/navigation";
+import {clearKeywords} from "@/app/redux/feature/product/api";
 import Searchbar from "@/components/searchBar/search";
 import Filter from "@/components/searchBar/filtersidebar";
-import { BsFilterLeft } from "react-icons/bs";
-import { getImage } from "@/app/redux/feature/product/image/api";
+import {BsFilterLeft} from "react-icons/bs";
+import {getImage} from "@/app/redux/feature/product/image/api";
 import Hero from "@/components/Home/gallary/Hero";
 
 const filterOptions = {
   sortBy: ["newest", "oldest", "popular"],
-  orientation: ["vertical", "horizontal"],
-  size: { imageHeight: 0, imageWidth: 0 },
-  density: 0,
-  fileType: ["jpeg", "png"],
+  imageOrientation: ["vertical", "horizontal"],
+  imageFileType: ["jpeg", "png"],
+  imageWidth: 0, // input
+  imageHeight: 0, // input
+  imageDensity: 0, // input
 };
 
 const Page = () => {
   const dispatch = useAppDispatch();
   const router = useRouter();
   const searchParams = useSearchParams();
-  const category = searchParams.get("category");
-  const [totalPages, setTotalPages] = useState(1);
-  const [currentPage, setCurrentPage] = useState(1);
-  const searchTerm = searchParams.get("searchTerm") || "";
+  const category = searchParams.get( "category" );
+  const [totalPages, setTotalPages] = useState( 1 );
+  const [currentPage, setCurrentPage] = useState( 1 );
+  const searchTerm = searchParams.get( "searchTerm" ) || "";
   const categoryParam = category ? ["editor choice"] : "";
-  const [isFilterOpen, setIsFilterOpen] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [filteredData, setFilteredData] = useState([]);
+  const [isFilterOpen, setIsFilterOpen] = useState( false );
+  const [loading, setLoading] = useState( false );
   const {
     imageData: product,
     imagePage,
     totalImageData,
     totalImageNumOfPage,
-  } = useAppSelector((state) => state.product);
+  } = useAppSelector( ( state ) => state.product );
+  const {user} = useAppSelector( ( state ) => state.user );
 
   const toggleFilter = () => {
-    setIsFilterOpen(!isFilterOpen);
+    setIsFilterOpen( !isFilterOpen );
   };
 
-  const handlePageChange = (page: number) => {
-    dispatch(setImagePage(page));
+  const handlePageChange = ( page: number ) => {
+    dispatch( setImagePage( page ) );
   };
 
   const handleNextPage = () => {
-    handlePageChange(imagePage === totalImageNumOfPage ? 1 : imagePage + 1);
+    handlePageChange( imagePage === totalImageNumOfPage ? 1 : imagePage + 1 );
   };
-  const { user } = useAppSelector((state) => state.user);
   const handlePrevPage = () => {
-    handlePageChange(imagePage === 1 ? totalImageNumOfPage : imagePage - 1);
+    handlePageChange( imagePage === 1 ? totalImageNumOfPage : imagePage - 1 );
   };
 
-  const fetchData = async (page: number) => {
-    setLoading(true);
-    const response = await getImage(dispatch, !!user, {
-      page: imagePage,
-      productsPerPage: 10,
+  const fetchData = async ( page: number ) => {
+    setLoading( true );
+    const filters = Object.fromEntries( searchParams );
+    const response = await getImage( dispatch, !!user, {
+      page,
       mediaType: ["image"],
       searchTerm,
       category: categoryParam,
-    });
-
-    setLoading(false);
+      productsPerPage: "10",
+      sortBy: filters.sortBy || "newest",
+      imageOrientation: filters.imageOrientation,
+      imageFileType: filters.imageFileType,
+      imageWidth: filters.imageWidth,
+      imageHeight: filters.imageHeight,
+      imageDensity: filters.imageDensity,
+    } );
+    setLoading( false );
   };
 
-  const fetchFilterData = async (
-    page: number,
-    filters: Record<string, string>
-  ) => {
-    setLoading(true);
-    try {
-      const response = await instance.get("/product/filter", {
-        params: {
-          page,
-          productsPerPage: 10,
-          mediaType: ["image"],
-          ...filters,
-        },
-      });
-      console.log("response in filtered data:-", response);
-      setFilteredData(response.data.products);
-      setLoading(false);
-    } catch (error) {
-      console.error("Error fetching filtered data:", error);
-      setLoading(false);
-    }
-  };
-
-  const handleFilterChange = (filterType: string, value: string | number) => {
-    const currentParams = new URLSearchParams(searchParams.toString());
-    currentParams.set(filterType, value.toString());
-    router.push(`?${currentParams.toString()}`, { scroll: false });
-    const filters = Object.fromEntries(currentParams);
-    fetchFilterData(imagePage, filters);
+  const handleFilterChange = ( filterType: string, value: string | number ) => {
+    const currentParams = new URLSearchParams( searchParams.toString() );
+    currentParams.set( filterType, value.toString() );
+    router.push( `?${currentParams.toString()}`, {scroll: false} );
+    fetchData( imagePage );
   };
 
   const handleClearFilters = () => {
-    const currentParams = new URLSearchParams(searchParams.toString());
-    currentParams.delete("sortBy");
-    currentParams.delete("orientation");
-    currentParams.delete("minHeight");
-    currentParams.delete("minWidth");
-    currentParams.delete("maxHeight");
-    currentParams.delete("maxWidth");
-    currentParams.delete("fileType");
-    currentParams.delete("minDensity");
-    currentParams.delete("maxDensity");
-    router.push(`?${currentParams.toString()}`, { scroll: false });
-    fetchData(imagePage);
+    const currentParams = new URLSearchParams( searchParams.toString() );
+    Object.keys( filterOptions ).forEach( ( key ) => {
+      currentParams.delete( key );
+    } );
+    router.push( `?${currentParams.toString()}`, {scroll: false} );
+    fetchData( imagePage );
   };
 
   const hasFilterParams = () => {
-    return Array.from(searchParams.keys()).some((key) =>
+    return Array.from( searchParams.keys() ).some( ( key ) =>
       [
         "sortBy",
-        "orientation",
-        "minHeight",
-        "minWidth",
-        "maxHeight",
-        "maxWidth",
-        "fileType",
-        "minDensity",
-        "maxDensity",
-      ].includes(key)
+        "imageOrientation",
+        "imageFileType",
+        "imageWidth",
+        "imageHeight",
+        "imageDensity"
+      ].includes( key )
     );
   };
 
-  useEffect(() => {
+  useEffect( () => {
     const filterParamsExist = hasFilterParams();
-    setIsFilterOpen(filterParamsExist);
-    if (hasFilterParams()) {
-      const filters = Object.fromEntries(searchParams);
-      fetchFilterData(imagePage, filters);
-    } else {
-      fetchData(imagePage);
-    }
+    setIsFilterOpen( filterParamsExist );
+    fetchData( imagePage );
     return () => {
-      clearKeywords(dispatch);
+      clearKeywords( dispatch );
     };
-  }, [imagePage, searchParams]);
+  }, [imagePage, searchParams] );
 
-  const displayData = hasFilterParams() ? filteredData : product;
+  const displayData = product;
 
   return (
     <div className="flex flex-col min-h-screen">
       <Searchbar />
       <div className="flex flex-1">
-        <Filter
+        {/* <Filter
           isOpen={isFilterOpen}
           onToggle={toggleFilter}
           filterOptions={filterOptions}
           onFilterChange={handleFilterChange}
           onclearFilter={handleClearFilters}
+        /> */}
+        <Filter
+          isOpen={isFilterOpen}
+          onToggle={toggleFilter}
+          mediaType="image" 
+          onFilterChange={handleFilterChange}
+          onClearFilter={handleClearFilters}
         />
         <div className={`flex-1 transition-all duration-300 ease-in-out `}>
           <div className="p-4">
@@ -170,9 +145,8 @@ const Page = () => {
             <div className="main items-center">
               <div className="bg-[#eeeeee]">
                 <div
-                  className={`py-10 lg:mx-4 ${
-                    !isFilterOpen ? "xl:mx-24 md:mx-4" : "ml-0"
-                  } `}
+                  className={`py-10 lg:mx-4 ${!isFilterOpen ? "xl:mx-24 md:mx-4" : "ml-0"
+                    } `}
                 >
                   <h1 className="text-2xl font-bold  text-start">
                     Today's Trending Images
@@ -188,9 +162,9 @@ const Page = () => {
                     ) : (
                       <div className="columns-1 min-h-screen sm:columns-2 md:columns-3 lg:columns-4 gap-2 mt-2 relative">
                         {displayData.length > 0 ? (
-                          displayData.map((data: any) => (
+                          displayData.map( ( data: any ) => (
                             <ImageGallery key={data._id} data={data} />
-                          ))
+                          ) )
                         ) : (
                           <p>No Images found.</p>
                         )}
@@ -208,11 +182,10 @@ const Page = () => {
                     type="button"
                     disabled={currentPage === 1}
                     variant="flat"
-                    className={`${
-                      currentPage === 1
+                    className={`${currentPage === 1
                         ? "opacity-70 cursor-not-allowed"
                         : "hover:bg-webred"
-                    } bg-webred text-white rounded-full font-bold`}
+                      } bg-webred text-white rounded-full font-bold`}
                     onPress={handlePrevPage}
                   >
                     Prev
@@ -234,11 +207,10 @@ const Page = () => {
                     size="sm"
                     disabled={currentPage === totalPages}
                     variant="flat"
-                    className={`${
-                      currentPage === totalPages
+                    className={`${currentPage === totalPages
                         ? "opacity-70 cursor-not-allowed"
                         : "hover:bg-webred"
-                    } bg-webred text-white rounded-full font-bold`}
+                      } bg-webred text-white rounded-full font-bold`}
                     onPress={handleNextPage}
                   >
                     Next
